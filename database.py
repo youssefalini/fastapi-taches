77d57b564d2da26a1e82ac7d5ca1b4644008711a
Youssef ALINI <youssefalini1@gmail.com>
2026-02-02 18:35:12 +0100

    Securisation avec variables d'environnement

--- database.py
+++ database.py
@@ -18,12 +18,24 @@
 # # 4. La Base (Le patron)
 # # Toutes nos futures tables (Tâches, Utilisateurs...) hériteront de cette classe.
 # Base = declarative_base()
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_DoiZRdE0nl8J@ep-wild-rice-ahh9n18w-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+# 1. On charge le fichier .env (s'il existe, donc sur ton PC)
+load_dotenv()
+# 2. On récupère la variable sécurisée.
+# Si on est sur Koyeb, il la trouvera dans ses réglages.
+# Si on est sur PC, il la trouvera dans le fichier .env
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Petit filet de sécurité : si la variable est vide, on prévient
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError(
+        "Pas de DATABASE_URL trouvée ! Vérifiez vos variables d'environnement."
+    )
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
